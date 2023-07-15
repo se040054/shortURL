5f54866ea078f5d9c7ce3558addf1b64a3ec0bd9
se040054 <peter8822967@gmail.com>
2023-07-15 19:48:03 +0800

    add : search render home / empty / search /

--- app.js
+++ app.js
@@ -14,9 +14,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/shortURL', (req, res) => {
-  res.render('home')
+  
+  const fullURL = req.query.transform?.trim();
+  let search= "none" //一開始沒有設置搜尋
+  if (typeof(fullURL)==='string'){
+    search = fullURL.length>0 ? "yes" : "empty" //當fullURL被宣告 檢測字串長度
+  }
+  if (search==="none"){res.render('home' , {fullURL})}
+  else if (search==="yes"){res.render('search' , {fullURL})}
+  else if (search==="empty")res.render('empty')
 })
 
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Click : http://localhost:3000/shortURL`)
 })
